ca0c323b842e2061391a291cebfd216dac427c51
Matías Barrios <matias.barriosn@gmail.com>
2020-12-25 14:36:15 -0300

    initial commit

---
+++ packages/luckytester/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'luckytester',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/luckytester',
+};
